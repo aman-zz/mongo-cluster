a1e229cf33dfe011950586306d7a0e4cbe8c11d0
AMANSHR <rio113114@gmail.com>
2020-06-29 18:53:35 +0000

    third

--- init-mongo.js
+++ init-mongo.js
@@ -1,12 +1,12 @@
-db.createUser(
-	{
-		user	:	"test"
-		pwd	:	"test"
-		role	:	[
-			{
-				role	:	"readWrite"
-				db	:	"test"
-			}
-		]
-	}
-)
+use admin
+db.createUser( {
+     user: "siteUserAdmin",
+     pwd: "password",
+     roles: [ { role: "userAdminAnyDatabase", db: "admin" } ]
+   });
+db.createUser( {
+     user: "siteRootAdmin",
+     pwd: "password",
+     roles: [ { role: "root", db: "admin" } ]
+   });
+rs.initiate();
--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@ set -xe
 openssl rand -base64 741 > mongodb-keyfile
 chmod 600 mongodb-keyfile
 sudo chown 999 mongodb-keyfile
-docker-compose up
+#docker-compose up
